a98f97c13a2e4b1c6d2f9a8a65e2cf393ee5bd31
William Lin <willlin1996@gmail.com>
2020-01-29 19:22:37 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 board = [" "," "," "," "," "," "," "," "," "]
+<<<<<<< HEAD
 def turn_count(board)
   counter = 0
 board.each do |player_symbol|
@@ -16,3 +17,15 @@ else
   "O"
 end
 end
+=======
+def turn_count(board, counter)
+board.each do |player_symbol|
+  counter = 0
+ if  player_symbol == "X" || player_symbol == "O"
+    counter += 1 
+    "#{counter += 1}"
+end
+end
+end
+
+>>>>>>> e6e57d757a9fcbd4de490a7aef8ad941a898d96c
